06d3d6e2c35f85dbe752dddb0079483069cc54ba
Daniel Siepmann <coding@daniel-siepmann.de>
2021-02-01 09:36:41 +0100

    Allow installation within TYPO3

---
+++ ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'ThüCAT',
+    'description' => 'Integration of ThüCAT into TYPO3 CMS.',
+    'category' => 'fe',
+    'state' => 'beta',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author' => 'Daniel Siepmann',
+    'author_email' => 'coding@daniel-siepmann.de',
+    'author_company' => '',
+    'version' => '0.1.0',
+    'constraints' => [
+        'depends' => [
+            'core' => '',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
