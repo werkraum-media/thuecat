89a076808ae9a5fb2fbf11fe18ab11fe873269b0
Daniel Siepmann <coding@daniel-siepmann.de>
2021-02-01 09:12:41 +0100

    Add CGL

---
+++ ecs.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use PhpCsFixer\Fixer\ArrayNotation\TrailingCommaInMultilineArrayFixer;
+use PhpCsFixer\Fixer\Import\FullyQualifiedStrictTypesFixer;
+use PhpCsFixer\Fixer\PhpUnit\PhpUnitTestAnnotationFixer;
+use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
+use PhpCsFixer\Fixer\StringNotation\SingleQuoteFixer;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $services = $containerConfigurator->services();
+
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/Classes',
+        __DIR__ . '/Tests',
+        __DIR__ . '/ecs.php',
+        __DIR__ . '/ext_emconf.php',
+    ]);
+
+    $parameters->set(Option::SETS, [
+        SetList::PSR_12,
+        SetList::PHP_70,
+        SetList::PHP_73_MIGRATION,
+        SetList::PHPUNIT,
+    ]);
+
+    $parameters->set(Option::SKIP, [
+        DeclareStrictTypesFixer::class => [
+            __DIR__ . '/ext_emconf.php',
+        ],
+    ]);
+
+    $services->set(FullyQualifiedStrictTypesFixer::class);
+    $services->set(PhpUnitTestAnnotationFixer::class)->call('configure', [[
+        'style' => 'annotation',
+    ]]);
+    $services->set(ArraySyntaxFixer::class)->call('configure', [[
+        'syntax' => 'short',
+    ]]);
+    $services->set(SingleQuoteFixer::class);
+    $services->set(TrailingCommaInMultilineArrayFixer::class);
+};
