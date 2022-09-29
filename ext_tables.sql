5447b95c0a0d8d8759fffda350ecd87708cf3c9f
Daniel Siepmann <coding@daniel-siepmann.de>
2022-09-29 15:17:06 +0200

    Show last import time beside each import configuration in backend module (#77)

--- ext_tables.sql
+++ ext_tables.sql
@@ -6,6 +6,7 @@ CREATE TABLE tx_thuecat_import_configuration (
     title varchar(255) DEFAULT '' NOT NULL,
     type varchar(255) DEFAULT '' NOT NULL,
     configuration text,
+    logs int(11) unsigned DEFAULT '0' NOT NULL,
 );
 
 CREATE TABLE tx_thuecat_import_log (
