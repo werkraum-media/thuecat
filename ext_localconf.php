d9a7153c0581572b6b0a7486a4c04b1778f08ee0
Daniel Siepmann <coding@daniel-siepmann.de>
2023-01-03 10:42:33 +0100

    Configure EXT:scheduler table garbage collection (#102)
    Add configuration to clean up garbage of old import records.
    
    Relates: #10244

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,4 +8,16 @@ defined('TYPO3') or die();
     TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
         '@import "EXT:' . $extensionKey . '/Configuration/TypoScript/Default/Setup.typoscript"'
     );
+
+    $tablesForCleanup = [
+        'tx_thuecat_import_log',
+        'tx_thuecat_import_log_entry',
+    ];
+
+    foreach ($tablesForCleanup as $tableName) {
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\TYPO3\CMS\Scheduler\Task\TableGarbageCollectionTask::class]['options']['tables'][$tableName] = [
+            'dateField' => 'crdate',
+            'expirePeriod' => '180',
+        ];
+    }
 })(\WerkraumMedia\ThueCat\Extension::EXTENSION_KEY);
